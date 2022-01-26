5cfaa113561d4ac564fdacfc196109cae219f5b8
AnneManzhura <manzhurik@gmail.com>
2022-01-26 16:24:39 +0200

    screenshots upload on fail added

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -17,7 +17,7 @@ const confReporter = {
     },
     reportSeleniumCommands: false, // add selenium commands to log
     seleniumCommandsLogLevel: 'debug', // log level for selenium commands
-    autoAttachScreenshots: false, // automatically add screenshots
+    autoAttachScreenshots: true, // automatically add screenshots
     screenshotsLogLevel: 'info', // log level for screenshots
     parseTagsFromTestTitle: false, // parse strings like `@foo` from titles and add to Report Portal
     cucumberNestedSteps: false, // report cucumber steps as Report Portal steps
@@ -287,14 +287,13 @@ export const config: WebdriverIO.Config = {
      * @param {Boolean} result.passed    true if test has passed, otherwise false
      * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    /*afterTest(test) {
-        if (test.passed === false) {
-            const filename = "screnshot.png";
-            const outputFile = path.join(__dirname, filename);
-            browser.saveScreenshot(outputFile);
-            reportportal.sendFileToTest(test, 'info', filename, fs.readFileSync(outputFile));
-        }
-    }*/
+    async afterTest(test) {
+        const { title, fullTitle } = test;
+
+        const screenshot = await browser.takeScreenshot();
+        const attachment = Buffer.from(screenshot, 'base64');
+        reportportal.sendFileToTest({ title }, 'info', 'screenshot.png', attachment);
+    }
 
 
     /**
